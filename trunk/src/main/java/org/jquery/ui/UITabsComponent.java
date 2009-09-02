feb229b62243041f0162340ad0f60133f7aef49b
mvillalobos@kineteque.com <mvillalobos@kineteque.com@d0474508-971e-11de-be30-e1f2c9f5b6b5>
2009-09-02 08:06:42 +0000

    tabs and dialog now working.

--- trunk/src/main/java/org/jquery/ui/UITabsComponent.java
+++ trunk/src/main/java/org/jquery/ui/UITabsComponent.java
@@ -5,5 +5,8 @@ import javax.faces.component.UINamingContainer;
 
 @FacesComponent(value = "org.jquery.ui.TabsComponent")
 public class UITabsComponent extends UINamingContainer {
-
+    @Override
+    public String getFamily() {
+        return "javax.faces.Panel";
+    }
 }
